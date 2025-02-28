79cf112d1998cdd822246e3fc78cdd0182d4ecc6
cost12 <51209490+cost12@users.noreply.github.com>
2025-02-28 13:58:53 -0500

    update Room model

--- main/code/models/room.py
+++ main/code/models/room.py
@@ -14,19 +14,54 @@ class Direction(Enum):
     UP        = 8
     DOWN      = 9
 
+class Exit:
+    """Base class for an Exit that leads from one Room to another Room
+    """
+    def __init__(self, name:str, description:str, end:'Room'):
+        self.name = name
+        self.description = description
+        self.end = end
+
+    def get_name(self) -> str:
+        """Get the Exit's name
+
+        :return: The Exit's name
+        :rtype: str
+        """
+        return self.name
+    
+    def get_description(self) -> str:
+        """Get the Exit's description
+
+        :return: The Exit's description
+        :rtype: str
+        """
+        return self.description
+    
+    def get_end(self) -> 'Room':
+        """Get the Room at the end of the Exit
+
+        :return: The Room at the end of the Exit
+        :rtype: Room
+        """
+        return self.end
+
 class Room:
     """Base class for rooms that a Character can move between
     """
-    def __init__(self, name:str, description:str):
-        """Create a Room
+    def __init__(self, name:str, description:str, exits:dict[Direction,Exit]):
+        """Creates a Room
 
         :param name: The name of the Room
         :type name: str
         :param description: A description of the room
+        :param exits: A dict from the directions to the Exits
+        :type exits: dict[Direction, Exit]
         :type description: str
         """
         self.name = name
         self.description = description
+        self.exits = exits
 
     def get_name(self) -> str:
         """Get the Room's name
@@ -43,13 +78,11 @@ class Room:
         :rtype: str
         """
         return self.description
+    
+    def get_exits(self) -> dict[Direction, Exit]:
+        """Get the Exits from the Room
 
-class Layout:
-    """A layout of rooms that a Character can move between
-    """
-
-    def __init__(self):
-        """Creates a Layout
+        :return: The exits from the Room
+        :rtype: dict[Direction, Exit]
         """
-        self.layout = dict[Room, dict[Direction,Room]]()
-        
+        return self.exits
