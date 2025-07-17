6d0fd229c028164e976559d28d6b4bb6c04cda9c
Miles Harrison <milesch9@gmail.com>
2025-07-16 22:56:50 -0400

    Bongo and Brown Book

---
+++ main/data/aagame1/items/BrownBook.py
@@ -0,0 +1,33 @@
+from utils.relator import NameFinder
+from models.actors import Target
+from models.response import StaticResponse
+from readin.description_helpers import plain_text
+from readin.utils import sdg_from_parts
+
+def add_to_name_space(name_space:NameFinder) -> None:
+    sdg = sdg_from_parts(
+        unbreakable_states=[name_space.get_from_id("visible",   "state")],
+        state_graphs      =[name_space.get_from_id("takeable",  "stategraph"),
+                            name_space.get_from_id("flammable", "stategraph")],
+        breakable_states  =[name_space.get_from_id("readable",  "state")],
+        name_space        =name_space
+    )
+    
+    brown_book = Target(
+        name="Brown Book",
+        aliases=["book"],
+        description=(plain_text, "a plain brown book"),
+        states=sdg,
+        weight=1,
+        value=1,
+        size=1,
+        target_responses={
+            name_space.get_from_id("take", "action") : StaticResponse("You take the Brown Book."),
+            name_space.get_from_id("read", "action") : StaticResponse("You try to read the book, but its script is one which you can neither recognize nor decipher.")
+        },
+        state_responses={
+            name_space.get_from_id("broken", "state") : StaticResponse("You rip the book in two, destroying its readability. Nice going...")
+        }
+    )
+
+    name_space.add(brown_book)
